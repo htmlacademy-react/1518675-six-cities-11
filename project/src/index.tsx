10676bbc0599ddc223bc12f3a4ac8a101c4be684
gladosq <gladosq@gmail.com>
2022-10-22 13:00:11 +0400

    added components for main page

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
+import App from './components/App/App';
+
+const mockPrices = [
+  { id: 1, price: 100 },
+  { id: 2, price: 789 },
+  { id: 3, price: 122 },
+  { id: 4, price: 32 },
+  { id: 5, price: 666 },
+  { id: 6, price: 69 }
+];
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
-
 root.render(
   <React.StrictMode>
-    <App />
+    <App prices={mockPrices}/>
   </React.StrictMode>,
 );
